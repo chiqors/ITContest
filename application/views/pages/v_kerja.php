8a44382961be0c2811c39330cedb08200edbb42b
HAN-105 <faster105>
2018-07-08 14:16:18 +0700

    Update v_kerja.php (data perusahaan)

--- application/views/pages/v_kerja.php
+++ application/views/pages/v_kerja.php
@@ -6,47 +6,41 @@
         <table id="mytable" class="table table-bordred table-striped">
           <thead>
             <th><input type="checkbox" id="checkall" /></th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Address</th>
-            <th>Email</th>
-            <th>Contact</th>
-            <th>Edit</th>
-            <th>Delete</th>
+            <th>Nama Perusahaan</th>
+            <th>Jenis Perusahaan</th>
+            <th>Status Rekrut</th>
+            <th>Alamat</th>
+            <th>Aksi</th>
           </thead>
-        <tbody>
-          <tr>
-            <td><input type="checkbox" class="checkthis" /></td>
-            <td>Mohsin</td>
-            <td>Irshad</td>
-            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-            <td>isometric.mohsin@gmail.com</td>
-            <td>+923335586757</td>
-            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-          </tr>
-          <tr>
-            <td><input type="checkbox" class="checkthis" /></td>
-            <td>Mohsin</td>
-            <td>Irshad</td>
-            <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-            <td>isometric.mohsin@gmail.com</td>
-            <td>+923335586757</td>
-            <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-            <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="clearfix"></div>
-      <ul class="pagination pull-right">
-        <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
-        <li class="active"><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
-      </ul>
+          <tbody>
+            <tr>
+              <td><input type="checkbox" class="checkthis" /></td>
+              <td>PT. Artha</td>
+              <td>Informasi/Media</td>
+              <td>Terbuka</td>
+              <td>Jl. Sari Gantung no. 2</td>
+              <td><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></td>
+            </tr>
+            <tr>
+              <td><input type="checkbox" class="checkthis" /></td>
+              <td>PT. Artha</td>
+              <td>Informasi/Media</td>
+              <td>Terbuka</td>
+              <td>Jl. Sari Gantung no. 2</td>
+              <td><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="clearfix"></div>
+        <ul class="pagination pull-right">
+          <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span></a></li>
+          <li class="active"><a href="#">1</a></li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">4</a></li>
+          <li><a href="#">5</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span></a></li>
+        </ul>
       </div>
     </div>
   </div>
