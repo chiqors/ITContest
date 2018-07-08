46a45016030568ecb6283eb10048b483133bf981
HAN-105 <faster105>
2018-07-08 11:03:02 +0700

    update

--- application/views/layouts/v_footer.php
+++ application/views/layouts/v_footer.php
@@ -1,7 +1,6 @@
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="<?= base_url() ?>/assets/plugins/jquery-3.3.1.min.js"></script>
-    <script src="<?= base_url() ?>/assets/plugins/popper.min.js"></script>
     <script src="<?= base_url() ?>/assets/bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
